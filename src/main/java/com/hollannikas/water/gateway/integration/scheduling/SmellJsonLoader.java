008fbc7c57bba0b5954b266a64d3a51ca690588e
hollannikas <rudolf.poels75@gmail.com>
2024-01-26 14:05:09 +0200

    Externalize vesla URL

--- src/main/java/com/hollannikas/water/gateway/integration/scheduling/SmellJsonLoader.java
+++ src/main/java/com/hollannikas/water/gateway/integration/scheduling/SmellJsonLoader.java
@@ -6,9 +6,12 @@ import com.hollannikas.water.domain.usecase.SmellLoadGateway;
 import com.hollannikas.water.gateway.integration.dto.SmellDataJsonDTO;
 import com.hollannikas.water.gateway.integration.dto.SmellJsonDTO;
 import com.hollannikas.water.gateway.integration.mapper.SmellJsonDTOMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,19 +21,26 @@ public class SmellJsonLoader implements SmellLoadGateway {
 
     final List<SmellJsonDTO> smells = new ArrayList<>();
 
-    public SmellJsonLoader() {
-        // TODO: read from API and insert into DB
-        var om = new ObjectMapper();
-        var url = getClass().getClassLoader().getResource("haju.json");
+    final String veslaUrl;
+
+    public SmellJsonLoader(@Value("${vesla.url}") String veslaUrl) {
+        this.veslaUrl = veslaUrl;
+    }
+
+    private void fetchSmellsFromOData() {
         try {
-            var data = om.readValue(url, SmellDataJsonDTO.class);
-            this.smells.addAll(Arrays.asList(data.getSmellEntities()));
-        } catch (IOException e) {
-            // Log
+            var url = new URI(veslaUrl).toURL();
+            var data = new ObjectMapper().readValue(url, SmellDataJsonDTO.class);
+            var smells = Arrays.asList(data.getSmellEntities());
+            this.smells.addAll(smells);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
+
     @Override
     public List<Smell> loadSmells() {
+        fetchSmellsFromOData();
         return smells.stream()
                 .map(SmellJsonDTOMapper::from)
                 .toList();
